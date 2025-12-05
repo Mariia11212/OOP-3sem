4189b541f3f9f9281e7d5603865f7fb588336aa0
Mariia11212 <yanytskamaria@gmail.com>
2025-12-05 22:40:11 +0200

    feat: Add Lab 3 (CryptoMonitor)

---
+++ lab3/CryptoMonitor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoMonitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
